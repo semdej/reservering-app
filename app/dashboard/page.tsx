156fd5b3a0d070630da6cfcdda0bd0de08eeec38
MeneerSDJ <semdejong04@gmail.com>
2024-02-14 14:11:13 +0100

    Add table

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,15 @@ import { Database } from "../database.types";
 import { redirect } from "next/navigation";
 import Navbar from "../components/Navbar";
 import { ReserveForm } from "../components/ReserveForm";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableCaption,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
 
 export default async function Dashboard() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -12,6 +21,11 @@ export default async function Dashboard() {
     data: { session },
   } = await supabase.auth.getSession();
 
+  const { data: reservations, error } = await supabase
+    .from("reservations")
+    .select("*")
+    .order("date", { ascending: true });
+
   if (!session) {
     redirect("/");
   } else {
@@ -19,6 +33,33 @@ export default async function Dashboard() {
       <>
         <Navbar />
         <ReserveForm />
+        {reservations ? (
+          <Table>
+            <TableCaption>Reserveringen</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Datum</TableHead>
+                <TableHead>Tijd</TableHead>
+                <TableHead>Kamer</TableHead>
+                <TableHead>Acties</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {reservations.map((reservation) => (
+                <TableRow key={reservation.id}>
+                  <TableCell>{reservation.date}</TableCell>
+                  <TableCell>{reservation.room}</TableCell>
+                  <TableCell>{reservation.description}</TableCell>
+                  <TableCell>
+                    <button>Delete</button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        ) : (
+          <p>Loading...</p>
+        )}
       </>
     );
   }
