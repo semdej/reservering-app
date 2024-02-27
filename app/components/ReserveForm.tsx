12425e4cf1021cb74ad4613f31223d336368d480
semdej <51273508+semdej@users.noreply.github.com>
2024-02-27 17:01:27 +0100

    Code cleanup

--- app/components/ReserveForm.tsx
+++ app/components/ReserveForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
@@ -28,11 +27,6 @@ import {
   FormMessage,
 } from "./ui/form";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
 
 const FormSchema = z.object({
   date: z.date({
