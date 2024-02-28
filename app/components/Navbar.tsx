c0a8601887ef09fdd6c343774f60f98ecc8ce358
MeneerSDJ <semdejong04@gmail.com>
2024-02-28 13:27:08 +0100

    Add dropdown for settings

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function Navbar() {
   return (
@@ -15,17 +23,30 @@ export default function Navbar() {
             Reservations
           </Button>
         </Link>
-        <Link href="/account" legacyBehavior passHref>
-          <Button className="m-2" variant="outline">
-            Account
-          </Button>
-        </Link>
       </div>
-      <form action="/auth/signout" method="post">
-        <Button className="m-2" variant="outline">
-          Uitloggen
-        </Button>
-      </form>
+      <div className="flex gap-x-6 m-3">
+        <DropdownMenu>
+          <DropdownMenuTrigger>Instellingen</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              <Link href="/account" legacyBehavior passHref>
+                Mijn Account
+              </Link>
+            </DropdownMenuLabel>
+            <DropdownMenuLabel>
+              <Link href="/team" legacyBehavior passHref>
+                Mijn Team
+              </Link>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <form action="/auth/signout" method="post">
+                <button>Uitloggen</button>
+              </form>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 }
