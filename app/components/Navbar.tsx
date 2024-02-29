f1c7470a08595e54eb6c8f83dd0653a5f108935e
semdej <51273508+semdej@users.noreply.github.com>
2024-02-29 16:45:20 +0100

    Navbar update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,50 +9,55 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 
+import { Separator } from "./ui/separator";
+
 import { CiSettings } from "react-icons/ci";
 
 export default function Navbar() {
   return (
-    <div className="flex gap-x-6 m-3 justify-between">
-      <div>
-        <Link href="/dashboard" legacyBehavior passHref>
-          <Button className="m-2" variant="outline">
-            Dashboard
-          </Button>
-        </Link>
-        <Link href="/reservations" legacyBehavior passHref>
-          <Button className="m-2" variant="outline">
-            Reservations
-          </Button>
-        </Link>
-      </div>
-      <div className="flex gap-x-6 m-3">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost">
-              <CiSettings size={25} />
+    <>
+      <div className="flex gap-x-6 m-2 justify-between">
+        <div>
+          <Link href="/dashboard" legacyBehavior passHref>
+            <Button className="m-2" variant="ghost">
+              Dashboard
             </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>
-              <Link href="/account" legacyBehavior passHref>
-                Mijn Account
-              </Link>
-            </DropdownMenuLabel>
-            <DropdownMenuLabel>
-              <Link href="/team" legacyBehavior passHref>
-                Mijn Team
-              </Link>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <form action="/auth/signout" method="post">
-                <button>Uitloggen</button>
-              </form>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+          </Link>
+          <Link href="/reservations" legacyBehavior passHref>
+            <Button className="m-2" variant="ghost">
+              Reservations
+            </Button>
+          </Link>
+        </div>
+        <div className="flex gap-x-6 m-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" variant="ghost">
+                <CiSettings size={25} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>
+                <Link href="/account" legacyBehavior passHref>
+                  Mijn Account
+                </Link>
+              </DropdownMenuLabel>
+              <DropdownMenuLabel>
+                <Link href="/team" legacyBehavior passHref>
+                  Mijn Team
+                </Link>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <form action="/auth/signout" method="post">
+                  <button>Uitloggen</button>
+                </form>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
-    </div>
+      <Separator className="mb-5 mt-0 " />
+    </>
   );
 }
