98e6a81329607e94fae15d035abc744250671a16
MeneerSDJ <semdejong04@gmail.com>
2024-03-11 17:01:51 +0100

    Update auth-form.tsx

--- app/login/auth-form.tsx
+++ app/login/auth-form.tsx
@@ -31,7 +31,7 @@ export default function AuthForm() {
             theme="dark"
             showLinks={false}
             providers={[]}
-            redirectTo="http://localhost:3000/auth/callback"
+            redirectTo="https://reservering-app.vercel.app/auth/callback"
           />
         </CardContent>
       </Card>
