bd33407823e20496fb21b169e312f8dfb17d2739
MeneerSDJ <semdejong04@gmail.com>
2024-03-13 11:02:26 +0100

    Add filter for rooms

--- app/reservations/DataTable.tsx
+++ app/reservations/DataTable.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import React from "react";
+
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
@@ -18,6 +22,7 @@ import {
 } from "../components/ui/table";
 
 import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -28,15 +33,33 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
   });
 
   return (
     <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter kamers..."
+          value={(table.getColumn("room")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("room")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
